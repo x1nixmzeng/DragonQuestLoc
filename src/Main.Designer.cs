016a0e307e46ef2c75a88ed0d9dd9027303c7d6d
x1nixmzeng <x1nixmzeng@live.co.uk>
2016-04-24 15:29:52 +0100

    Added search box to filter lines
    Also show the reset confirmation when opening a new file

--- src/Main.Designer.cs
+++ src/Main.Designer.cs
@@ -42,10 +42,22 @@
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripContainer2 = new System.Windows.Forms.ToolStripContainer();
+            this.toolStripContainer3 = new System.Windows.Forms.ToolStripContainer();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
             this.menuStrip1.SuspendLayout();
             this.toolStripContainer1.ContentPanel.SuspendLayout();
-            this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
             this.toolStripContainer1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.toolStripContainer2.ContentPanel.SuspendLayout();
+            this.toolStripContainer2.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer2.SuspendLayout();
+            this.toolStripContainer3.ContentPanel.SuspendLayout();
+            this.toolStripContainer3.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer3.SuspendLayout();
             this.SuspendLayout();
             // 
             // listView1
@@ -62,7 +74,7 @@
             this.listView1.Location = new System.Drawing.Point(0, 0);
             this.listView1.MultiSelect = false;
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(752, 327);
+            this.listView1.Size = new System.Drawing.Size(752, 302);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -146,24 +158,92 @@
             // 
             this.toolStripContainer1.ContentPanel.AutoScroll = true;
             this.toolStripContainer1.ContentPanel.Controls.Add(this.listView1);
-            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(752, 327);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(752, 302);
             this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
             this.toolStripContainer1.Name = "toolStripContainer1";
-            this.toolStripContainer1.Size = new System.Drawing.Size(752, 351);
+            this.toolStripContainer1.Size = new System.Drawing.Size(752, 302);
             this.toolStripContainer1.TabIndex = 3;
             this.toolStripContainer1.Text = "toolStripContainer1";
             // 
-            // toolStripContainer1.TopToolStripPanel
+            // toolStrip1
             // 
-            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.menuStrip1);
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripTextBox1,
+            this.toolStripLabel2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(752, 25);
+            this.toolStrip1.Stretch = true;
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(36, 22);
+            this.toolStripLabel1.Text = "Filter:";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.MaxLength = 128;
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(200, 25);
+            this.toolStripTextBox1.TextChanged += new System.EventHandler(this.toolStripTextBox1_TextChanged);
+            // 
+            // toolStripContainer2
+            // 
+            // 
+            // toolStripContainer2.ContentPanel
+            // 
+            this.toolStripContainer2.ContentPanel.AutoScroll = true;
+            this.toolStripContainer2.ContentPanel.Controls.Add(this.toolStripContainer1);
+            this.toolStripContainer2.ContentPanel.Size = new System.Drawing.Size(752, 302);
+            this.toolStripContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer2.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer2.Name = "toolStripContainer2";
+            this.toolStripContainer2.Size = new System.Drawing.Size(752, 327);
+            this.toolStripContainer2.TabIndex = 5;
+            this.toolStripContainer2.Text = "toolStripContainer2";
+            // 
+            // toolStripContainer2.TopToolStripPanel
+            // 
+            this.toolStripContainer2.TopToolStripPanel.Controls.Add(this.toolStrip1);
+            // 
+            // toolStripContainer3
+            // 
+            // 
+            // toolStripContainer3.ContentPanel
+            // 
+            this.toolStripContainer3.ContentPanel.AutoScroll = true;
+            this.toolStripContainer3.ContentPanel.Controls.Add(this.toolStripContainer2);
+            this.toolStripContainer3.ContentPanel.Size = new System.Drawing.Size(752, 327);
+            this.toolStripContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer3.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer3.Name = "toolStripContainer3";
+            this.toolStripContainer3.Size = new System.Drawing.Size(752, 351);
+            this.toolStripContainer3.TabIndex = 6;
+            this.toolStripContainer3.Text = "toolStripContainer3";
+            // 
+            // toolStripContainer3.TopToolStripPanel
+            // 
+            this.toolStripContainer3.TopToolStripPanel.Controls.Add(this.menuStrip1);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(86, 22);
+            this.toolStripLabel2.Text = "toolStripLabel2";
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(752, 351);
-            this.Controls.Add(this.toolStripContainer1);
+            this.Controls.Add(this.toolStripContainer3);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Main";
@@ -173,10 +253,20 @@
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.toolStripContainer1.ContentPanel.ResumeLayout(false);
-            this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
-            this.toolStripContainer1.TopToolStripPanel.PerformLayout();
             this.toolStripContainer1.ResumeLayout(false);
             this.toolStripContainer1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.toolStripContainer2.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer2.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer2.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer2.ResumeLayout(false);
+            this.toolStripContainer2.PerformLayout();
+            this.toolStripContainer3.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer3.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer3.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer3.ResumeLayout(false);
+            this.toolStripContainer3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -196,6 +286,12 @@
         private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer2;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer3;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
     }
 }
 
--- src/Main.cs
+++ src/Main.cs
@@ -13,8 +13,19 @@ namespace DragonQuestLoc
 {
     public partial class Main : Form
     {
-        MesDatatype LastMes;
+        struct MesListItem
+        {
+            public bool visible;
+            public string key;
+            public string original;
+            public string translated;
 
+            public string search_data;
+        }
+
+        MesDatatype CurrentMesFile;
+        MesListItem[] CurrentMesItems;
+        
         public Main()
         {
             InitializeComponent();
@@ -22,7 +33,8 @@ namespace DragonQuestLoc
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            LastMes = null;
+            CurrentMesFile = null;
+            CurrentMesItems = null;
 
             ResetUI();
             ResetTitle();
@@ -32,7 +44,7 @@ namespace DragonQuestLoc
         {
             string default_name = "Dragon Quest Monsters: Joker 3";
 
-            if( (LastMes != null ) && fn.Length > 0 )
+            if ((CurrentMesFile != null) && fn.Length > 0)
             {
                 Text = string.Format("{0} - {1}", Path.GetFileName(fn), default_name);
             }
@@ -44,29 +56,76 @@ namespace DragonQuestLoc
 
         private void ResetUI()
         {
+            listView1.BeginUpdate();
             listView1.Items.Clear();
 
-            if (LastMes != null)
+            if (CurrentMesItems != null)
             {
-                var mes_items = LastMes.GetAllValues();
-
-                foreach (Tuple<string, string> pair in mes_items)
+                foreach (MesListItem item in CurrentMesItems )
                 {
-                    var new_item = listView1.Items.Add(pair.Item1).SubItems;
-                    new_item.Add(pair.Item2);   // original value
-                    new_item.Add("");           // modified value
+                    if( item.visible )
+                    {
+                        var new_item = listView1.Items.Add(item.key).SubItems;
+                        new_item.Add(item.original);        // original value
+                        new_item.Add(item.translated);      // modified value
+                    }
                 }
-
+                
                 exportToolStripMenuItem.Enabled = true;
             }
             else
             {
                 exportToolStripMenuItem.Enabled = false;
             }
+
+            listView1.EndUpdate();
+
+            toolStripLabel2.Text = "";
+
+            if (CurrentMesItems != null)
+            {
+                int num = listView1.Items.Count;
+                int den = CurrentMesItems.Length;
+
+                if (toolStripTextBox1.Text.Length > 0)
+                {
+                    toolStripLabel2.Text = string.Format("{0} of {1} items", num, den);
+                }
+            }
+        }
+
+        private int GetVisibleIndex(int idx)
+        {
+            if (CurrentMesItems != null)
+            {
+                int it = 0;
+                int i = 0;
+                while (i < CurrentMesItems.Length)
+                {
+                    if (CurrentMesItems[i].visible)
+                    {
+                        if (idx == it)
+                        {
+                            return i;
+                        }
+
+                        ++it;
+                    }
+
+                    ++i;
+                }
+            }
+
+            return -1;
         }
 
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            if (!ConfirmAction("load another file"))
+            {
+                return;
+            }
+
             if( openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK )
             {
                 string fn1 = openFileDialog1.FileNames[0];
@@ -82,7 +141,29 @@ namespace DragonQuestLoc
                         var mes = new MesDatatype();
                         mes.Read(br);
 
-                        LastMes = mes;
+                        CurrentMesFile = mes;
+                        CurrentMesItems = null;
+
+                        var list_items = new List<MesListItem>();
+                        var mes_items = CurrentMesFile.GetAllValues();
+
+                        foreach (Tuple<string, string> pair in mes_items)
+                        {
+                            var li = new MesListItem();
+
+                            li.visible = true;
+                            li.key = pair.Item1;
+                            li.original = pair.Item2;
+                            li.translated = "";
+
+                            // keywords for searching
+                            li.search_data = string.Concat(li.key.ToLower(), "\0", li.original.ToLower());
+
+                            list_items.Add(li);
+                        }
+
+                        CurrentMesItems = list_items.ToArray();
+
                         ResetUI();
                         ResetTitle(fn1);
                     }
@@ -96,24 +177,30 @@ namespace DragonQuestLoc
         {
             if( listView1.SelectedIndices.Count == 1 )
             {
-                int index = listView1.SelectedIndices[0];
+                int index = GetVisibleIndex(listView1.SelectedIndices[0]);
+
+                if (index == -1)
+                {
+                    return;
+                }
 
                 EditDialog ed = new EditDialog();
 
-                var single = LastMes.GetSingleValue(index);
+                var single = CurrentMesFile.GetSingleValue(index);
 
                 ed.Setup(single.Item1, single.Item2);
 
-                if( ed.ShowDialog() == System.Windows.Forms.DialogResult.OK )
+                if (ed.ShowDialog() == System.Windows.Forms.DialogResult.OK)
                 {
                     var result = ed.UserInput();
 
-                    if( LastMes.UpdateValue(index, result.Item1, result.Item2) )
+                    if (CurrentMesFile.UpdateValue(index, result.Item1, result.Item2))
                     {
-                        listView1.SelectedItems[0].SubItems[2].Text = result.Item2;
+                        CurrentMesItems[index].translated = result.Item2;
+
+                        ResetUI();
                     }
                 }
-
             }
         }
 
@@ -122,27 +209,39 @@ namespace DragonQuestLoc
             Close();
         }
 
-        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        private bool ConfirmAction(string action)
         {
-            if( LastMes != null )
+            bool quit_allowed = true;
+
+            if (CurrentMesFile != null)
             {
-                int edit_count = LastMes.NumEdits();
+                int edit_count = CurrentMesFile.NumEdits();
 
                 if (edit_count > 0)
                 {
-                    string saver_str = string.Format("You have made changes to {0} line(s)\n\nAre you sure you want to quit?", edit_count);
+                    string saver_str = string.Format("You have made changes to {0} line(s)\n\nAre you sure you want to {1}?", edit_count, action);
 
-                    if( MessageBox.Show(saver_str, "Warning!", MessageBoxButtons.YesNo, MessageBoxIcon.Question) != System.Windows.Forms.DialogResult.Yes)
+                    if (MessageBox.Show(saver_str, "Warning!", MessageBoxButtons.YesNo, MessageBoxIcon.Question) != System.Windows.Forms.DialogResult.Yes)
                     {
-                        e.Cancel = true;
+                        quit_allowed = false;
                     }
                 }
+            }
+
+            return quit_allowed;
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!ConfirmAction("quit"))
+            {
+                e.Cancel = true;
             }
         }
 
         private void exportToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if( LastMes != null )
+            if (CurrentMesFile != null)
             {
                 if( saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
                 {
@@ -155,7 +254,7 @@ namespace DragonQuestLoc
                         {
                             var bw = new BinaryWriter(fs);
 
-                            LastMes.Write(bw);
+                            CurrentMesFile.Write(bw);
 
                             ResetUI();
 
@@ -164,6 +263,40 @@ namespace DragonQuestLoc
                     }
                 }
             }
+        }
+
+        private void toolStripTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if( CurrentMesItems == null )
+            {
+                return;
+            }
+
+            var box = sender as ToolStripTextBox;
+
+            if(box == null)
+            {
+                return;
+            }
+
+            string query = box.Text.ToLower();
+
+            if (query.Length == 0)
+            {
+                for (int i = 0; i < CurrentMesItems.Length; ++i)
+                {
+                    CurrentMesItems[i].visible = true;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < CurrentMesItems.Length; ++i)
+                {
+                    CurrentMesItems[i].visible = CurrentMesItems[i].search_data.Contains(query);
+                }
+            }
+
+            ResetUI();
         }
     }
 }
